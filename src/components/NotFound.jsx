8e076f62bafc54e5ac990dcb9c275c101a498c95
Syed Haider <shayder426@gmail.com>
2020-08-24 02:58:14 -0700

    Update design of NotFound page

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,24 +1,54 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Box from "@material-ui/core/Box";
+import Typography from "@material-ui/core/Typography";
+import makeStyles from "@material-ui/core/styles/makeStyles";
+import Container from "@material-ui/core/Container";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
 
-/* NotFound page is routed to when user goes to an invalid url */
+import Header from "./header-components/Header";
 
-const NotFound = () => {
-  document.body.style.cursor = "default";
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  title: {
+    margin: theme.spacing(4),
+  },
+  button: {
+    "& > *": {
+      margin: theme.spacing(1),
+    },
+  },
+}));
+
+export default function NotFound(props) {
+  const classes = useStyles(props);
+
   return (
-    <React.Fragment>
-      <div className="container ">
-        <div className="home-display ">
-          <div className="col-sm-6 margin-center">
-            <h1 className="text-center">Page Not Found</h1>
-            <div className="text-center jumbotron ">
-              <h2>Oops!</h2>
-              <p>Visit our homepage to browse through our site</p>
-            </div>
-          </div>
+    <Container component="main" maxWidth="xs" border={1}>
+      <CssBaseline />
+      <Header></Header>
+      <Box border={1}>
+        <div className={classes.paper}>
+          <Avatar className={classes.avatar}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h5" variant="h6">
+            {"Page not found!!!"}
+          </Typography>
         </div>
-      </div>
-    </React.Fragment>
+      </Box>
+    </Container>
   );
-};
-
-export default NotFound;
+}
